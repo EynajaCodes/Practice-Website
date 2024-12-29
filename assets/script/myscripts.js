fe1f0ad569a78b97d12a6f2eb4eb2373a931ef23
Wizdomami <138355859+EynajaCodes@users.noreply.github.com>
2024-12-29 02:57:36 -0500

    adding some media query
    adding some media query

---
+++ assets/script/myscripts.js
@@ -0,0 +1,18 @@
+
+
+//Global Variables
+
+let tweep= document.querySelectorAll("tweepcontainer");
+let tweepArray = [];//Array that holds tweeps as they're done
+let navLink= document.querySelectorAll('.navbar a');
+
+
+
+
+tweepArray.forEach(tweep => {
+
+   let newTweep =
+
+    
+});
+
